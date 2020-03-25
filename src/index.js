ecdd73c316e35b8144b9d787f0828b075730b606
Catharina Mesquita <catha.ana.1994@gmail.com>
2020-03-25 19:32:22 -0300

    Adicionando e configurando knexjs

--- src/index.js
+++ src/index.js
@@ -1,10 +1,9 @@
 const express = require('express')
-
+const routes = require('./routes')
 const app = express()
 
-app.get('/', (req, res) => {
-  return res.json({ hello: 'world' })
-})
+app.use(express.json())
+app.use(routes)
 
 app.listen(3333, () => {
   console.info('Server runing: http://localhost:3333')
