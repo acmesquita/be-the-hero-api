443e3615edafcec1c6d90ce7810024047d1ecf42
Catharina Mesquita <catha.ana.1994@gmail.com>
2020-03-25 20:21:48 -0300

    Criando controller de ongs

---
+++ src/controllers/OngControlller.js
@@ -0,0 +1,25 @@
+const db = require('../database/connection-db')
+const crypto = require('crypto')
+
+module.exports = {
+  async store(req, res) {
+    const ongs = await db('ongs').select('*')
+
+    return res.json(ongs)
+  },
+
+  async create(req, res) {
+    const { name, email, whatsapp, city, uf } = req.body
+    const id = crypto.randomBytes(4).toString('HEX')
+
+    await db('ongs').insert({
+      id,
+      name,
+      email,
+      whatsapp,
+      city,
+      uf
+    })
+    return res.json({ id })
+  }
+}
