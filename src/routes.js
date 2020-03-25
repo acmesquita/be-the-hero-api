583bce64defb1b9d22980b7b9dc3e7c470d538da
Catharina Mesquita <catha.ana.1994@gmail.com>
2020-03-25 20:15:17 -0300

    Criando conexão com o banco e criando rotas para criação e listagem de ongs

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,28 @@
 const express = require('express')
+const crypto = require('crypto')
+const db = require('./database/connection-db')
 
 const routes = express.Router();
 
-routes.get('/', (req, res) => {
-  return res.json({ hello: 'world' })
+routes.get('/ongs', async (req, res) => {
+  const ongs = await db('ongs').select('*')
+
+  return res.json(ongs)
+})
+
+routes.post('/ongs', async (req, res) => {
+  const { name, email, whatsapp, city, uf } = req.body
+  const id = crypto.randomBytes(4).toString('HEX')
+
+  await db('ongs').insert({
+    id,
+    name,
+    email,
+    whatsapp,
+    city,
+    uf
+  })
+  return res.json({ id })
 })
 
 module.exports = routes
