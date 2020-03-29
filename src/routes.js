8d34a2c1bb6e90e63e0a27c3bdc37a18b7006830
Catharina Mesquita <catha.ana.1994@gmail.com>
2020-03-29 16:46:00 -0300

    Adicionando validação nas rotas

--- src/routes.js
+++ src/routes.js
@@ -1,5 +1,6 @@
 const express = require('express')
-const routes = express.Router();
+const routes = express.Router()
+const { celebrate, Segments, Joi } = require('celebrate')
 
 const OngController = require('./controllers/OngControlller')
 const IncindentController = require('./controllers/IncindentController')
@@ -9,12 +10,33 @@ const SessionController = require('./controllers/SessionController')
 routes.post('/sessions', SessionController.create)
 
 routes.get('/ongs', OngController.index)
-routes.post('/ongs', OngController.create)
 
-routes.get('/incidents', IncindentController.index)
+routes.post('/ongs', celebrate({
+  [Segments.BODY]: Joi.object().keys({
+    name: Joi.string().required(),
+    email: Joi.string().required().email(),
+    whatsapp: Joi.number().required().min(10).max(11),
+    city: Joi.string().required(),
+    uf: Joi.string().required().length(2)
+  })
+}), OngController.create)
+
+routes.get('/incidents', celebrate({
+  [Segments.QUERY]: Joi.object().keys({
+    page: Joi.number().min(1).positive()
+  })
+}), IncindentController.index)
 routes.post('/incidents', IncindentController.create)
-routes.delete('/incidents/:id', IncindentController.delete)
+routes.delete('/incidents/:id', celebrate({
+  [Segments.PARAMS]: Joi.object().keys({
+    id: Joi.number().required(),
+  })
+}), IncindentController.delete)
 
-routes.get('/profile', ProfileController.index)
+routes.get('/profile', celebrate({
+  [Segments.HEADERS]: Joi.object({
+    authorization: Joi.string().required()
+  }).unknown(),
+}), ProfileController.index)
 
 module.exports = routes
